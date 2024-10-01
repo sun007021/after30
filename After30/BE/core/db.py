08876b4c94267ba02bfe0f78ef7513f5226f2d4f
Sunwook Lee <85978148+sun007021@users.noreply.github.com>
2024-10-01 13:50:00 +0000

    chore: change db info

--- After30/BE/core/db.py
+++ After30/BE/core/db.py
@@ -7,12 +7,12 @@ from sqlalchemy.ext.asyncio import (
 import contextlib
 
 engine = create_async_engine(
-    'postgresql+asyncpg://sesiadmin:sesi1234@localhost:5432/sesidb')
+    "postgresql+asyncpg://after30adm:after30!@localhost:5432/after30db"
+)
 
 
 # 세션 생성기
-SessionLocal = async_sessionmaker(
-    autocommit=False, autoflush=False, bind=engine)
+SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
